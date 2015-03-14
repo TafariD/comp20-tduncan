6e1c535206d5d24f6cfa88689983a60227390d7b
Tafari_D <tafari.duncan@tufts.edu>
2015-03-14 00:01:13 -0400

    Not much is working at this moment.

--- mmap/java.js
+++ mmap/java.js
@@ -1,35 +1,114 @@
-/*var xmlhttp = new XMLHttpRequest(); 
-var url = "data.json";
 
-xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        getLocation;
-        var myArr = JSON.parse(xmlhttp.responseText);
+// This code is originally sourced from Ming.
+
+
+      var myLat = 0;
+      var myLng = 0;
+      var request = new XMLHttpRequest();
+
+      var me = new google.maps.LatLng(myLat, myLng);
+      var myOptions = {
+            zoom: 13, // The larger the zoom number, the bigger the zoom
+            center: me,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+          };
+      var map;
+      var marker;
+      var infowindow = new google.maps.InfoWindow();
+      var places;
+
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.setRequestHeader("https", "https://secret-about-box.herokuapp.com/sendLocation"); 
+      var url = "https://secret-about-box.herokuapp.com/sendLocation";
+
+      xmlhttp.onreadystatechange = function() {
+          if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+              var myArr = JSON.parse(xmlhttp.responseText);
+              myFunction(myArr);
+          }
+      }
+
+      xmlhttp.open("GET", url, true);
+      xmlhttp.send();
+
+      function myFunction(thingy) {
+          for(i = 0; i < thingy.length; i++) {
         
-    }
-}
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
+        ppl = new google.maps.LatLng(thingy[i].lat, thingy[i].lng);
+  
+        // Create a marker
+        marker = new google.maps.Marker({
+          position: ppl,
+          title: thingy[i].login;
+        });
+        marker.setMap(map);
+        });
+      
+      function init()
+      {
+        map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+        console.log("Call before getMyLocation()");
+        getMyLocation();
+        console.log("Call after getMyLocation()");
+      }
+      
+      function getMyLocation() {
+        console.log("In getMyLocation()");
+        if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+          navigator.geolocation.getCurrentPosition(function(position) {
+            myLat = position.coords.latitude;
+            myLng = position.coords.longitude;
+            renderMap();
+          });
+        }
+        else {
+          alert("Geolocation is not supported by your web browser.  What a shame!");
+        }
+        console.log("Leaving getMyLocation()");
+      }
 
-
-var x = document.getElementById("demo"); */
-
- function initialize() {
-    var mapOptions = {
-        center: { lat: -34.397, lng: 150.644},
-        zoom: 8
+      function renderMap()
+      {
+        me = new google.maps.LatLng(myLat, myLng);
+        
+        // Update map and go there...
+        map.panTo(me);
+  
+        // Create a marker
+        marker = new google.maps.Marker({
+          position: me,
+          title: "Here I Am!"
+        });
+        marker.setMap(map);
+          
+        // Open info window on click of marker
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(marker.title);
+          infowindow.open(map, marker);
+        });
+        
+        // Calling Google Places API
+        var request = {
+          location: me,
+          radius: '500',
+          types: ['food']
         };
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
- }
- 
- google.maps.event.addDomListener(window, 'load', initialize);
-
-/*
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}  */
+        service = new google.maps.places.PlacesService(map);
+        service.search(request, callback);
+      }
+      
+      // Taken from http://code.google.com/apis/maps/documentation/javascript/places.html
+      function callback(results, status)
+      {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          alert("Got places back!");
+          places = results;
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
+        }
+      }
+      
+      function createMarker(place)
+      {
+      }
